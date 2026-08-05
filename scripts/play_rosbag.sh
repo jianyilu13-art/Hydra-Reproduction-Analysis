525c8d42c54b80aac698c667a99874514de399cc
lujianyi <jianyilu13@gmail.com>
2026-08-05 23:10:20 +0800

    add files

--- scripts/play_rosbag.sh
+++ scripts/play_rosbag.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ros2 bag play ~/datasets/uhumans2/office \
+ros2 bag play ~/datasets/uhumans2/uHumans2_office_s1_00h_v2 \
 --clock \
 --qos-profile-overrides-path ~/.tf_overrides.yaml
